7efb2a00428b94dac6bff2c0eef7b0295642022b
Yuki Ino <tensai.osaru25035@gmail.com>
2017-10-12 16:37:09 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,12 +3,12 @@ class MicropostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
 
   def create
-    @micropost = current_user.feed.microposts.build(micropost_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order('created_at DESC').page(params[:page])
+      @microposts = current_user.feed_microposts.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
@@ -19,13 +19,13 @@ class MicropostsController < ApplicationController
     flash[:success] = 'メッセージを削除しました。'
     redirect_back(fallback_location: root_path)
   end
-  
+
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
   end
-  
+
   def correct_user
     @micropost = current_user.microposts.find_by(id: params[:id])
     unless @micropost
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
+
 class Micropost < ApplicationRecord
   belongs_to :user
-  
+
   validates :user_id, presence: true
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 255 }
 end
